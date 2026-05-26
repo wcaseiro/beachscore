31bc2dc2d63f3f32e41639260670fed71295ec06
Wladimir Caseiro <wladimirfernando@gmail.com>
2026-05-26 16:54:20 -0300

    Corrige inicializacao HTTPS no Flask

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ app = create_app()
 if __name__ == "__main__":
     ssl_args = {}
     if Path("cert.pem").exists() and Path("key.pem").exists():
-        ssl_args = {"certfile": "cert.pem", "keyfile": "key.pem"}
+        ssl_args = {"ssl_context": ("cert.pem", "key.pem")}
 
     socketio.run(app, host="0.0.0.0", port=5000, **ssl_args)
